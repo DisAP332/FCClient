e2cab86b45346999130b30b6a1fbc0c394224379
DAP <disap332@gmail.com>
2023-07-31 14:12:46 -0400

    added server IP

--- src/Api/index.tsx
+++ src/Api/index.tsx
@@ -1,12 +1,9 @@
 import axios from 'axios'
 
-// const api = axios.create({
-//     baseURL: 'http://144.202.6.35:80'
-// })
-
 const api = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'http://144.202.6.35:80'
 })
+
 
 /// BEGIN EVENT HELPERS ///
 
