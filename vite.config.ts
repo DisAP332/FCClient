4d44b5eca911103c89b768fb8f380a00d74ad7bc
DAP <disap332@gmail.com>
2023-07-31 22:04:33 -0400

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/FCClient',
+  base: '/FCClient/',
   plugins: [react()],
   build: {
     target: 'esnext',
